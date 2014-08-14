6899849b010fffedc2c06455952ae41c2a1c6c1c
Adam Moehrig <adam@m7ws.com>
2014-08-14 14:39:09 -0500

    replaced the mysql_real_escape_string function with $this->db->escape for compatibility

--- catalog/model/opentshirts/product_region.php
+++ catalog/model/opentshirts/product_region.php
@@ -54,7 +54,7 @@ class ModelOpentshirtsProductRegion extends Model {
 			$data['asc_desc'] = ' ASC ';
 		}
 		
-		$sql .= $tables.$condition." ORDER BY ".mysql_real_escape_string($data['order'])." ".mysql_real_escape_string($data['asc_desc']);
+		$sql .= $tables.$condition." ORDER BY ".$this->db->escape($data['order'])." ".$this->db->escape($data['asc_desc']);
 		
 		return $sql;
 	}
